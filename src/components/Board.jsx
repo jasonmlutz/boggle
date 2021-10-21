5a57bc67ab8af62d67a050b6b8fa03b7bdeaeaf5
Jason Lutz <jason.m.lutz@gmail.com>
2021-10-21 15:17:12 -0700

    Add resources; improve useState
    This commit reorganizes the file structure to place the
    cube letters information in components/resources, along with
    the (now possibly un-needed) useSingleton custom hook.
    
    This commit also replaces the useSingleton usage in the Board
    component by instead setting an initial value of the
    state using useState. Similarly, the Cube component
    splits its given letters string and selects a displayed
    letter randomly on initial render using useState.

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,8 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import { useSingleton } from "./resources/singleton";
+
 import Cube from "./Cube";
+import { cubeLetters } from "./resources/cubeLetters";
 
 export default function Board() {
+  // try using https://reactjs.org/docs/hooks-reference.html#lazy-initial-state instead
+  const [cubeOrder, setCubeOrder] = useState(() => {
+    let currentIndex = cubeLetters.length,
+      randomIndex;
+    // While there remain elements to shuffle...
+    while (currentIndex != 0) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      // And swap it with the current element.
+      [cubeLetters[currentIndex], cubeLetters[randomIndex]] = [
+        cubeLetters[randomIndex],
+        cubeLetters[currentIndex],
+      ];
+    }
+
+    return cubeLetters;
+  });
+
+  // build a ul of Cube components from the cubeLetters
+  const orderedCubes = cubeOrder.map((letters, index) => (
+    <li>
+      <Cube key={index} letters={letters} />
+    </li>
+  ));
+
   return (
-    null
+    <ol className = "Board">{orderedCubes}</ol>
   )
 }
--- src/components/Cube.jsx
+++ src/components/Cube.jsx
@@ -3,10 +3,11 @@ import React, {useState} from "react";
 import { useSingleton } from "./resources/singleton";
 
 export default function Cube({ letters }) {
-  const [letter, setLetter] = useState("")
+  const [letter, setLetter] = useState(() => {
+    const splitLetters = letters.split(" ");
+    const initialLetter = splitLetters[Math.floor(Math.random() * letters.length)];
 
-  useSingleton(() => {
-    setLetter(letters[Math.floor(Math.random() * letters.length)])
+    return initialLetter
   })
 
   return <div className="Cube">{letter}</div>;
