700ad6b47ab2291a8ec652637adba2810f5acead
Jason Lutz <jason.m.lutz@gmail.com>
2021-12-28 15:31:18 -0800

    Add dictionary and word validation

--- src/components/WordList.jsx
+++ src/components/WordList.jsx
@@ -1,9 +1,20 @@
 import React from "react";
+import { dictionary } from "./resources/OWL2";
 
-export default function WordList({wordList}) {
-  const wordListItems = wordList.map((word, index) => (
-    <li key={index} className = "Word">{word}</li>
-  ));
+export default function WordList({ wordList }) {
+  const wordListItems = wordList.map((word, index) => {
+    var classname = "Word";
+    if (dictionary[word.toLowerCase()] === undefined) {
+      classname += " invalidWord";
+    } else {
+      classname += " validWord";
+    }
+    return (
+      <li key={index} className={classname}>
+        {word}
+      </li>
+    );
+  });
 
   return <ul className="WordList">{wordListItems}</ul>;
 }
