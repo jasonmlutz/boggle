9b800359f13e9d5c76a039001d766ab3f70ad6f5
Jason Lutz <jason.m.lutz@gmail.com>
2021-12-29 08:25:55 -0800

    Add definition display
    Clicking on a word in the word list will now console.log the word's
    definition(s), or 'no such word' in the case that it is an
    invalid word.

--- src/components/WordList.jsx
+++ src/components/WordList.jsx
@@ -4,13 +4,21 @@ import { dictionary } from "./resources/OWL2";
 export default function WordList({ wordList }) {
   const wordListItems = wordList.map((word, index) => {
     var classname = "Word";
-    if (dictionary[word.toLowerCase()] === undefined) {
+    var definition = dictionary[word.toLowerCase()];
+    if (definition === undefined) {
       classname += " invalidWord";
     } else {
       classname += " validWord";
     }
+    function displayDefinition() {
+      if (definition === undefined) {
+        console.log("no such word")
+      } else {
+        console.log(definition)
+      }
+    }
     return (
-      <li key={index} className={classname}>
+      <li key={index} className={classname} onClick = {displayDefinition}>
         {word}
       </li>
     );
